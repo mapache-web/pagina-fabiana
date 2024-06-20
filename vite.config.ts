5f85108ae3b05000737425ccd5bc02b0b9393caf
Fedomus <federico.liousin@gmail.com>
2024-06-19 21:41:32 -0300

    se corrige configuración de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
 
 // https://vitejs.dev/config/
-export default {
-  plugins: [react()],
-}
+export default defineConfig({
+  base: "/pagina-fabiana/",
+  plugins: [react()]
+});
